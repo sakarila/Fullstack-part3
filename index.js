bb8f799c4d358a859471b41dc35f5998e920cb2e
Sakari <sakari.laulajainen@tuni.fi>
2020-04-29 18:36:32 +0300

    User input validation for duplicate values added.

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/api/info', (req, res, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if (!body.name || !body.number) {
@@ -61,9 +61,12 @@ app.post('/api/persons', (req, res) => {
         name: body.name,
         number: body.number
     })
-    person.save().then(savedPerson => {
+    person.save()
+    .then(savedPerson => {
         res.json(savedPerson.toJSON());
     })
+    .catch(error => next(error))
+
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
@@ -95,8 +98,9 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
     next(error)
 }
 
